4e8eed5e8e0e36c91b9ef2e385b993b299c36f91
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-10-07 21:03:07 -0500

    Done Contacts Activity

--- permissionlocationmaps/app/src/main/java/com/example/permissionlocationmaps/ContactosActivity.java
+++ permissionlocationmaps/app/src/main/java/com/example/permissionlocationmaps/ContactosActivity.java
@@ -8,41 +8,48 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.ContactsContract;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class ContactosActivity extends AppCompatActivity {
 
-   static final int READ_CONTACTS_ID = 0;
-   TextView estadoPermiso;
+    String[] columnas;
+    Cursor cursor;
+    ContactosAdapter adaptador;
+    ListView listaContactos;
+
+   static final int READ_CONTACTS_ID = 1;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactos);
-        estadoPermiso = findViewById(R.id.estadoPermiso);
-
-        solicitarPermiso(this, Manifest.permission.READ_CONTACTS, "Necesito leer los contactos porfi", READ_CONTACTS_ID);
+        listaContactos = findViewById(R.id.listaContactos);
+        columnas = new String[]
+                {ContactsContract.Profile._ID,
+                 ContactsContract.Profile.DISPLAY_NAME_PRIMARY};
+        adaptador = new ContactosAdapter(this, null, 0);
+        listaContactos.setAdapter(adaptador);
+        solicitarPermiso(this, Manifest.permission.READ_CONTACTS, "Necesito los contactos porfi", READ_CONTACTS_ID);
         usarPermiso();
     }
 
     private void usarPermiso(){
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)== PackageManager.PERMISSION_GRANTED) {
-            //Aqui va el codigo con riesgo
-            estadoPermiso.setText("Permiso concedido");
-            estadoPermiso.setTextColor(Color.GREEN);
-        } else {
-            estadoPermiso.setText("Permiso rechazaco");
-            estadoPermiso.setTextColor(Color.RED);
+            cursor=getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                    columnas, null, null, null);
+
+            adaptador.changeCursor(cursor);
         }
     }
 
     private void solicitarPermiso(Activity context, String permiso, String justificacion, int idPermiso){
         if (ContextCompat.checkSelfPermission(context ,permiso)!= PackageManager.PERMISSION_GRANTED) {
             if(ActivityCompat.shouldShowRequestPermissionRationale(context, permiso)) {
-                Toast.makeText(this, justificacion, Toast.LENGTH_LONG).show();
             }
 
             ActivityCompat.requestPermissions(context, new String[]{permiso}, idPermiso);
