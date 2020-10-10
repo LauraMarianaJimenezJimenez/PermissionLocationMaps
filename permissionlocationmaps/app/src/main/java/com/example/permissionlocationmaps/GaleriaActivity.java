56754f738dc98c9c5d583da6e5ec2e22d85ad255
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-10-10 16:28:27 -0500

    Added Gallery Function

--- permissionlocationmaps/app/src/main/java/com/example/permissionlocationmaps/GaleriaActivity.java
+++ permissionlocationmaps/app/src/main/java/com/example/permissionlocationmaps/GaleriaActivity.java
@@ -1,14 +1,66 @@
 package com.example.permissionlocationmaps;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class GaleriaActivity extends AppCompatActivity {
+
+    static final int IMAGE_PICKER_REQUEST = 0;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    Button btnGaleria, btnCamara;
+    ImageView imgFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeria);
+
+        btnGaleria = findViewById(R.id.btnGaleria);
+        btnCamara = findViewById(R.id.btnCamara);
+        imgFoto = findViewById(R.id.imgFoto);
+
+        btnGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent pickImage = new Intent(Intent.ACTION_PICK);
+                pickImage.setType("image/*");
+                startActivityForResult(pickImage,IMAGE_PICKER_REQUEST);
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case IMAGE_PICKER_REQUEST:
+                if(resultCode == RESULT_OK){
+                    try {
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        imgFoto.setImageBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }
+    }
+
 }
